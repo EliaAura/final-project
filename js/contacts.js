c64e0759fd3521352bda0ca51a14fef00ae5280f
Ella <volatelis@gmail.com>
2022-05-17 10:56:55 +0300

    second commit

---
+++ js/contacts.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('contacts-form');
+
+function getFormValue(e) {
+    
+    e.preventDefault();
+
+    const name = form.querySelector('[name="name"]');
+    const mail = form.querySelector('[name="mail"]');
+    const phone = form.querySelector('[name="phone"]');
+    const message = form.querySelector('[name="message"]');
+
+    const values = {
+        name: name.value,
+        mail: mail.value,
+        phone: phone.value,
+        message: message.value
+    };
+
+    let fail = "";
+    if(name.value=="" || phone.value=="" || mail.value=="" || message.value=="") {
+    fail = "Заполните все поля";
+    } else if (name.value.length < 2) {
+    fail = "Введите правильно имя";
+    } else if (message.value.length < 20) {
+    fail = "Ваше сообщение слишком короткое";
+    };
+    if (fail!="") {
+    document.getElementById('error').innerHTML = fail;
+    return false;
+    } else {
+    alert("Спасибо за сообщение!");
+    console.log(values);
+    form.reset();
+    }
+}
+
+form.addEventListener('submit',getFormValue);
--- js/review.js
+++ js/review.js
@@ -9,3 +9,42 @@ ratingItemsArray.forEach(item =>
         //request back-end
     })
     )
+
+
+const form = document.querySelector('.review-send');
+
+function getFormValue(e) {
+    e.preventDefault();
+    const name = form.querySelector('[name="name"]');
+    const surname = form.querySelector('[name="surname"]');
+    const email = form.querySelector('[name="email"]');
+    const review = form.querySelector('[name="review"]');
+
+    const values = {
+        name: name.value,
+        surname: surname.value,
+        email: email.value,
+        review: review.value
+    };
+    
+    let fail = "";
+    if(name.value=="" || surname.value=="" || email.value=="" || review.value=="") {
+    fail = "Заполните все поля";
+    } else if (name.value.length < 2) {
+    fail = "Введите правильно имя";
+    }else if (surname.value.length < 2) {
+    fail = "Введите правильно фамилию";
+    } else if (review.value.length < 20) {
+    fail = "Ваше сообщение слишком короткое";
+    };
+    if (fail!="") {
+    document.getElementById('error').innerHTML = fail;
+    return false;
+    } else {
+    alert("Спасибо за отзыв!");
+    console.log(values);
+    form.reset();
+    }
+}
+
+form.addEventListener('submit',getFormValue);
